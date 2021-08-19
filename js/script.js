6c2080ecb3ae479bf84ac069d6a553688d42fa07
Nickosss97435 <nickosssanonymous@gmail.com>
2021-08-19 20:39:09 +0400

    repo6

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// form
+(function () {
+  emailjs.init("user_v6Xpjfmo2O6nM4jEy9SAj"); // Obtain your user ID at the dashboard https://dashboard.emailjs.com/integration
+})();
+
+// copy
+
+var btncopy = document.querySelector(".btn btn-full");
+if (btncopy) {
+  btncopy.addEventListener("click", docopy);
+}
+
+function docopy() {
+  // Cible de l'élément qui doit être copié
+  var target = this.dataset.target;
+  var fromElement = document.querySelector(target);
+  if (!fromElement) return;
+
+  // Sélection des caractères concernés
+  var range = document.createRange();
+  var selection = window.getSelection();
+  range.selectNode(fromElement);
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  try {
+    // Exécution de la commande de copie
+    var result = document.execCommand("copy");
+    if (result) {
+      // La copie a réussi
+      alert("Copié !");
+    }
+  } catch (err) {
+    // Une erreur est surevnue lors de la tentative de copie
+    alert(err);
+  }
+
+  // Fin de l'opération
+  selection = window.getSelection();
+  if (typeof selection.removeRange === "function") {
+    selection.removeRange(range);
+  } else if (typeof selection.removeAllRanges === "function") {
+    selection.removeAllRanges();
+  }
+}
---
+++ js/test.js
@@ -0,0 +1,6 @@
+function test(maFonction) {
+  alert("Test ok !!");
+}
+function test(maFonction) {
+  alert("Test ok !!");
+}
